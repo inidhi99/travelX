8c760c0f1ccf3950a1849276dba8f176f50c9dce
nidhi <inidhi99@gmail.com>
2022-12-09 14:07:06 -0500

    server/models/user.js

---
+++ server/models/User.js
@@ -0,0 +1,63 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// import post schema 
+
+const userSchema = new Schema(
+  {
+    firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // set the array of data that adheres to the userschema
+    following: [userSchema],
+    followers: [userSchema],
+    posts: [postSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+
+const User = model('User', userSchema);
+const Post = model('User', postSchema);
+
+module.exports = User;
