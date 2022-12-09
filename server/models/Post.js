31afd9bb92cc92cf355776b75cf5525ecd95aafc
nidhi <inidhi99@gmail.com>
2022-12-09 14:25:05 -0500

    server/models/post.js

---
+++ server/models/Post.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const postSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: 'You need to leave a thought!',
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  thoughtAuthor: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+ 
+});
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+module.exports = { User, Post };
