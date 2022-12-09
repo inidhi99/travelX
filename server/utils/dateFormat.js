4ec41110f07d5e76699050afbcb5ea15076052d5
Valeria <alarconmvaleria@gmail.com>
2022-12-09 13:54:48 -0500

    addDateSuffix created to add ordinals depending on the date

---
+++ server/utils/dateFormat.js
@@ -0,0 +1,18 @@
+const addDateSuffix = (date) => {
+    let dateStr = date.toString();
+  
+    // get last char of date string
+    const lastChar = dateStr.charAt(dateStr.length - 1);
+  
+    if (lastChar === '1' && dateStr !== '11') {
+      dateStr = `${dateStr}st`;
+    } else if (lastChar === '2' && dateStr !== '12') {
+      dateStr = `${dateStr}nd`;
+    } else if (lastChar === '3' && dateStr !== '13') {
+      dateStr = `${dateStr}rd`;
+    } else {
+      dateStr = `${dateStr}th`;
+    }
+  
+    return dateStr;
+  };
