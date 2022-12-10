617f63a431024e2d0c2aed44af000fdfc6e664ad
Jules Joseph <julesj93@gmail.com>
2022-12-10 01:44:35 -0500

    edited mutations /queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,3 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_COMMENTS = gql`
-  query getcomments {
-    comments {
-      _id
-      commentText
-      commentAuthor
-      createdAt
-    }
-  }
-`;
+
