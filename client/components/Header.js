0cd51a9b218368592ccd8dc9e3a8a2c1bc31a826
Igor Korman <ikorman12@gmail.com>
2022-12-10 02:32:25 -0500

    set up sign up/intial page for app with form, jumbotoron on top and footer stuck on bottom, app,jsx working appropriately

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,19 @@
+import Jumbotron from "react-bootstrap";
+
 function Header() {
   return (
     <>
-    <div>Header</div>
+      <Jumbotron className="header" styler={{ backgroundcolor: "lightblue" }}>
+        <h1 className="headerName" style={{ align: "center", color: "white" }}>
+          TravelX
+        </h1>
+        {/* <p>
+          <Button bsStyle="primary">Learn more</Button>
+        </p> */}
+      </Jumbotron>
+      ;
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- client/components/Main.js
+++ client/components/Main.js
@@ -1,9 +1,43 @@
+import { Form, FormGroup, FormControl } from "react-bootstrap";
+
 function Main() {
   return (
     <>
-    <div>Main</div>
+      <h2> Create a profile !</h2>
+      <Form horizontal>
+        <FormGroup controlId="formHorizontalEmail">
+          <Col componentClass={ControlLabel} sm={2}>
+            Email
+          </Col>
+          <Col sm={10}>
+            <FormControl type="email" placeholder="Email" />
+          </Col>
+        </FormGroup>
+
+        <FormGroup controlId="formHorizontalPassword">
+          <Col componentClass={ControlLabel} sm={2}>
+            Password
+          </Col>
+          <Col sm={10}>
+            <FormControl type="password" placeholder="Password" />
+          </Col>
+        </FormGroup>
+
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Checkbox>Remember me</Checkbox>
+          </Col>
+        </FormGroup>
+
+        <FormGroup>
+          <Col smOffset={2} sm={10}>
+            <Button type="submit">Sign in</Button>
+          </Col>
+        </FormGroup>
+      </Form>
+      ;
     </>
-  )
+  );
 }
 
-export default Main
+export default Main;
