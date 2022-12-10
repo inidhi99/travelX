7efb1b73a78afab28ddf298246f22e48f1ace2ea
Jules Joseph <julesj93@gmail.com>
2022-12-10 12:05:34 -0500

    creating user queries in utils

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,44 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_USERS = gql`
+  query allUsers {
+    profiles {
+      _id
+      firstName
+      lasName
+      username
+      following
+      follwers
+      posts
+    }
+  }
+`;
+
+export const QUERY_SINGLE_USER = gql`
+  query singleUSER($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      firstName
+      lasName
+      username
+      following
+      follwers
+      posts
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      firstName
+      lasName
+      username
+      following
+      follwers
+      posts
+    }
+  }
+`;
 
