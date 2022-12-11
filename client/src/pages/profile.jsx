4bc134fc1e7dab26d66517f1047f4fa80755c816
Valeria <alarconmvaleria@gmail.com>
2022-12-11 15:12:46 -0500

    checking data to see use of query me vs query single user

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -8,9 +8,13 @@ import { QUERY_SINGLE_USER, QUERY_ME, QUERY_POSTS } from '../utils/queries';
 const Profile = () => {
     const { username: userParam } = userParams();
 
+    // if there is no username in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's informtaion
     const { loading, data } = useQuery(userParam ? QUERY_SINGLE_USER : QUERY_ME, {
         variables: { username: userParam },
     });
+
+    // check if the data is returned from the `QUERY_ME` query, then the `QUERY_SINGLE_USER` query
+    const user = data?.me || data?.user | {};
 
     
 }
