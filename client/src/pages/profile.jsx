c0d76a8e4f69e207493a79bee2d58905a0c433f8
Valeria <alarconmvaleria@gmail.com>
2022-12-11 15:10:10 -0500

    created profile function and loading user queries for user's profile and their entered params

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -3,5 +3,14 @@ import { Navigate, useParams, Link } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import {  } from '../utils/mutations';
-import { QUERY_USRES, QUERY_SINGLE_USER, QUERY_POSTS } from '../utils/queries';
+import { QUERY_SINGLE_USER, QUERY_ME, QUERY_POSTS } from '../utils/queries';
 
+const Profile = () => {
+    const { username: userParam } = userParams();
+
+    const { loading, data } = useQuery(userParam ? QUERY_SINGLE_USER : QUERY_ME, {
+        variables: { username: userParam },
+    });
+
+    
+}
