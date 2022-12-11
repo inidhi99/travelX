274e780909c70febabb3f6a1598c53401db928c5
Valeria <alarconmvaleria@gmail.com>
2022-12-11 15:04:27 -0500

    imported useQuery

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Navigate, useParams, Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import {  } from '../utils/mutations';
 
