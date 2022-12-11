f07edd9be4407f5dff0d6f5d0a08880248e4a216
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-11 14:36:55 -0500

    package install and file struc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Main from "../components/Main";
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Main className='mainSection'/>
+      <Main className="mainSection" />
       <Footer />
     </div>
   );
