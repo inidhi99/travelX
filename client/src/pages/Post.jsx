79dd456abb939e54d51115bb8f04032b0c871b5a
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-11 17:24:39 -0500

    image post box added

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Image from 'react-bootstrap/Image';
 
-// Search photo
+// Post photo
 // add description
 // add loaction - country city
+// upload image
 // login and signup of not logged in
 // date
 // comments
@@ -12,15 +15,30 @@ import InputGroup from 'react-bootstrap/InputGroup';
 const Post = () => {
   return (
     <>
-      <p>This is the post page</p>
-      <>
+      <Container>
+        <p>This is the post page</p>
+        {/* Image section */}
+
+        <div style={{ display: 'block', width: 700, padding: 30 }}>
+          <h4>Image Goes Below</h4>
+          <Image
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210425000233/test-300x297.png"
+            roundedCircle
+          />
+        </div>
+
+        {/* Text area */}
         <InputGroup size="lg">
-          <InputGroup.Text>Write a caption...</InputGroup.Text>
+          <InputGroup.Text>Add Description</InputGroup.Text>
           <Form.Control as="textarea" aria-label="With textarea" />
         </InputGroup>
-        ;
-      </>
+        <InputGroup size="md">
+          <InputGroup.Text>Location</InputGroup.Text>
+          <Form.Control as="textarea" aria-label="With textarea" />
+        </InputGroup>
+      </Container>
     </>
   );
 };
+
 export default Post;
