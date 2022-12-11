5ad9af2823542c0638ef862c3aff7d10d48f0498
Valeria <alarconmvaleria@gmail.com>
2022-12-11 15:06:08 -0500

    inported queries

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -3,5 +3,5 @@ import { Navigate, useParams, Link } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import {  } from '../utils/mutations';
-
+import { QUERY_USRES, QUERY_SINGLE_USER, QUERY_POSTS } from '../utils/queries';
 
