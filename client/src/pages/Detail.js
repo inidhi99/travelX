bdb2720d418dcc99790d4fe7d7ebfff0a7df3341
Jules Joseph <julesj93@gmail.com>
2022-12-11 14:03:03 -0500

    restructured

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-
 import { useStoreContext } from '../utils/GlobalState';
 import {
  
