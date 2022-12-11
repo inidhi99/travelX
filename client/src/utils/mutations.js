4847d388f0e6671c9f0a918c1a483ebf4a7f5668
Jules Joseph <julesj93@gmail.com>
2022-12-11 17:32:11 -0500

    edited mutations and typeDefs for mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,3 +1,69 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_Post = gql`
+  mutation addPost($postText: String!) {
+    addPost(postText: $postText) {
+      _id
+      postText
+      postAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($postId: ID!, $commentText: String!) {
+    addComment(postId: $postId, commentText: $commentText) {
+      _id
+      postText
+      postAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const ADD_REACTION = gql`
+  mutation addReaction($postId: ID!, $reactionText: String!) {
+    addReaction(postId: $postId, reactionText: $reactionText) {
+      _id
+      userId
+      username
+      type
+      createdAt
+      }
+    }
+  }
+`;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,6 +16,8 @@ const typeDefs = gql`
   type Post {
     _id: ID
     userId: ID
+    postText: String
+    postAuthor: String
     country: String
     city: String
     comments: [Comment]!
@@ -28,6 +30,8 @@ const typeDefs = gql`
     _id: ID
     userId: ID
     username: String
+    commentText: String
+    commentAuthor: String
     content: String
     createdAt: String
   }
@@ -50,9 +54,16 @@ const typeDefs = gql`
     me: User
   }
 
-  // type Mutation {
- 
-  // }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addPost(postText: String!): Post
+    addComment(postId: ID!, commentText: String!): Post
+    addReaction(postId: ID!,): Post
+    removePost(postId: ID!): Post
+    removeComment(postId: ID!, commentId: ID!): Post
+    removeReaction (postId: ID!): Post
+  }
 `;
 
 module.exports = typeDefs;
