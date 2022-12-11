a6d948a3bdc34d2b664825e129f9001973560f3f
Jules Joseph <julesj93@gmail.com>
2022-12-11 17:34:29 -0500

    FORGOT TO MAKE POST ALL CAPS

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,7 +24,7 @@ export const ADD_USER = gql`
   }
 `;
 
-export const ADD_Post = gql`
+export const ADD_POST = gql`
   mutation addPost($postText: String!) {
     addPost(postText: $postText) {
       _id
