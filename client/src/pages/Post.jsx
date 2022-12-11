f34ce12b776519490a70645298e789b37d22ce98
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-11 15:14:50 -0500

    post page setup

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,3 +1,13 @@
 import React from 'react';
 
+// Search photo
+// add description
+// add loaction - country city
+// login and signup of not logged in
+// date
+// comments
+// reactions
+function Post() {
+  return <p>This is the post page</p>;
+}
 export default Post;
