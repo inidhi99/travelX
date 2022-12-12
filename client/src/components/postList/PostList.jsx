beef3d9c67ba50f601ec6c515b370054ad29b5c5
Valeria <alarconmvaleria@gmail.com>
2022-12-12 13:28:36 -0500

    PostList created to show posts in user profile

--- client/src/components/postList/PostList.jsx
+++ client/src/components/postList/PostList.jsx
@@ -1,2 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+const PostList = ({
+    posts
+}) => {
+    if (!posts.length) {
+        return <h3> No Posts Yet!</h3>
+    }
+    
+}
