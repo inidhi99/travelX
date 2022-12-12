50a94373d1f2702463a0a1e0c79cb401fe960b38
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-12 18:51:24 -0500

    img overflow fix

--- client/src/pages/postForm.jsx
+++ client/src/pages/postForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
+
 import Container from '@mui/material/Container';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
@@ -21,59 +21,20 @@ const itemData = [
     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
     title: 'Breakfast',
   },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
 ];
 
 const PostForm = () => {
   return (
     <>
       <CssBaseline />
-      <Container maxWidth="sm">
+      <Container maxWidth="md">
         {/* Image list  */}
-        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+
+        <ImageList
+          sx={{ width: 500, height: 450, overflow: 'hidden' }}
+          cols={1}
+          rowHeight={164}
+        >
           {itemData.map((item) => (
             <ImageListItem key={item.img}>
               <img
@@ -85,6 +46,7 @@ const PostForm = () => {
             </ImageListItem>
           ))}
         </ImageList>
+
         {/* Input Form */}
         <FormControl sx={{ width: '25ch' }}>
           <TextField
