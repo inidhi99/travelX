8e351b8531c3f798251e84124cede5a0a3a29f61
Igor Korman <ikorman12@gmail.com>
2022-12-12 02:50:43 -0500

    added list group for footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,8 +1,17 @@
+import ListGroup from "react-bootstrap/ListGroup";
 
 function Footer() {
   return (
     <>
-    
+      <ListGroup horizontal>
+        <ListGroup.Item>Igor</ListGroup.Item>
+        <ListGroup.Item>Jeff</ListGroup.Item>
+        <ListGroup.Item>Evince</ListGroup.Item>
+        <ListGroup.Item>Nidhi</ListGroup.Item>
+        <ListGroup.Item>Bria</ListGroup.Item>
+        <ListGroup.Item>Valeria</ListGroup.Item>
+        <ListGroup.Item>Jules</ListGroup.Item>
+      </ListGroup>
     </>
   );
 }
