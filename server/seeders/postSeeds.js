57926afe3103d1afa6088e6e1745e23730beed57
briabr <filalibria@yahoo.fr>
2022-12-12 13:00:41 -0500

    created postSeeds,Seed.js,userSeeds and edited some mistakes in the models

--- server/seeders/postSeeds.js
+++ server/seeders/postSeeds.js
@@ -1,44 +1,43 @@
-export default [
+module.exports = [
     {
         country:"United Kingdom",
-        city:"London",
+        city: "London",
         content: "I very much wanna visit this place",
-    }
+    },
+    {
+        country:"USA",
+        city:"New York",
+        content: "Splendid!",
+    },
+    {
+        country:"France",
+        city:"Paris",
+        content: "J'adore le paysage!",
+    },
+    {
+        country:"USA",
+        city:"Los Angeles",
+        content: "Splendid view!",
+    },
+    {
+        country:"Switzeland",
+        city:"Lausanne",
+        content: "The best place for Ski, love it!!",
+    },
+    {
+        country:"Japan",
+        city:"Kyoto",
+        content: "It's magical!",
+    },
+    {
+        country:"France",
+        city:"Nice",
+        content: "Beautiful churces and beaches",
+    },
+    {
+        country:"South Africa",
+        city:"Cape Town",
+        content: "Stunning nature!",
+    },
+
 ]
-
-//     {
-//         "commentText": "Love it!!",
-        
-//     },
-//     {
-//         "commentText": "I very much wanna visit this place",
-//         "User":"Edward Burrow "
-//     },
-
-//     {
-//         "commentText": "Great shot man!!",
-        
-    
-//     },
-
-//     {
-//         "commentText": "Splendid view!!",
-        
-//     },
-
-//     {
-//         "commentText": "Keep posting, love  your adventures!!",
-        
-//     },
-//     {
-//         "commentText": "This is definetly my next destination!!",
-        
-//     },
-//     {
-//         "commentText": "",
-        
-//     }
-    
-// ]
-
-
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,36 +1,86 @@
-const connection = require('../config/connection');
-const { User, Post } = require('../models');
-const posts = require('./postSeeds');
-const users = require('./userSeeds')
+const connection = require("../config/connection");
+const { User, Post } = require("../models");
+const posts = require("./postSeeds");
+const users = require("./userSeeds");
+// const comment = require('./commentSeeds')
+// const reactions = require('./reactionSeeds')
 
-connection.on('error', (err) => err);
+connection.on("error", (err) => err);
 
-connection.once('open', async () => {
-  console.log('connected');
+connection.once("open", async () => {
+  console.log("connected");
   await Post.deleteMany({});
   await User.deleteMany({});
 
-//   const users = [];
-//   const applications = getRandomApplications(10);
+  //   const users = [];
+  //   const applications = getRandomApplications(10);
 
-//   for (let i = 0; i < 20; i++) {
-//     const fullName = getRandomName();
-//     const first = fullName.split(' ')[0];
-//     const last = fullName.split(' ')[1];
+  //   for (let i = 0; i < 20; i++) {
+  //     const fullName = getRandomName();
+  //     const first = fullName.split(' ')[0];
+  //     const last = fullName.split(' ')[1];
 
-//     users.push({
-//       first,
-//       last,
-//       age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
-//     });
-//   }
+  //     users.push({
+  //       first,
+  //       last,
+  //       age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
+  //     });
+  //   }
 
   await User.collection.insertMany(users);
-//   await Post.collection.insertMany(applications);
+  const createdUsers = await User.find();
+  //   console.log(createdUsers)
+
+  const postsToInsert = [];
+  // go through each post datum
+  posts.map(async (post) => {
+    // get random user
+    const randomIndex = Math.floor(Math.random() * createdUsers.length);
+    const randomUser = createdUsers[randomIndex];
+    postsToInsert.push({
+      ...post,
+      userId: randomUser.userId,
+      username: randomUser.username,
+    });
+  });
+
+  await Post.collection.insertMany(postsToInsert);
+
+  const createdPosts = await Post.find();
+
+  // // go through each comment datum
+  // comments.map(async (comment) => {
+  //     // get random user
+  //     const randomIndex = Math.floor(Math.random() * createdPosts.length);
+  //     const randomPost = createdPosts[randomIndex];
+  //     const user = await User.findById(randomPost.userId);
+  //     await Comment.create({
+  //         ...comment,
+  //         postId: randomPost._id,
+  //         userId: randomPost.userId,
+  //         username: user.username
+  //     })
+  // })
+
+  // // go through each reaction datum
+  // reactions.map(async (reaction) => {
+  //     // get random user
+  //     const randomIndex = Math.floor(Math.random() * createdPosts.length);
+  //     const randomPost = createdPosts[randomIndex];
+  //     const user = await User.findById(randomPost.userId);
+  //     await Reaction.create({
+  //         ...reaction,
+  //         postId: randomPost._id,
+  //         userId: randomPost.userId,
+  //         username: user.username
+  //     })
+  // })
+
+  //   await Post.collection.insertMany(applications);
 
   // loop through the saved applications, for each application we need to generate a application response and insert the application responses
-  console.table(users);
-//   console.table(posts);
-  console.info('Seeding complete! 🌱');
+  console.log(createdUsers);
+  console.log(createdPosts);
+  console.info("Seeding complete! 🌱");
   process.exit(0);
 });
