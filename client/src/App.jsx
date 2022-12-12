ff5da2eeb283abb77533df9e8009053343d71623
nidhi <inidhi99@gmail.com>
2022-12-12 13:43:25 -0500

    updated the login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Explore from './pages/Explore'
 import Home from "./pages/Home";
 import Login from './pages/Login';
-import Signup from './pages/Signup';
+import SignupForm from './pages/SignupForm';
 
 
 function App() {
@@ -48,8 +48,8 @@ function App() {
                 element={<Login />} 
               />
               <Route 
-                path="/signup" 
-                element={<Signup />} 
+                path="/Signup" 
+                element={<SignupForm />} 
               />
               <Route 
                 path="/post" 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,71 +1,87 @@
+// see SignupForm.js for comments
 import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+// Import the `useMutation()` hook from Apollo Client
 import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-// import { LOGIN } from '../utils/mutations';
+// Import the GraphQL mutation
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
-function Login(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [login, { error }] = useMutation(LOGIN);
+const LoginForm = () => {
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [loginUser] = useMutation(LOGIN_USER)
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const mutationResponse = await login({
-        variables: { email: formState.email, password: formState.password },
-      });
-      const token = mutationResponse.data.login.token;
-      Auth.login(token);
-    } catch (e) {
-      console.log(e);
-    }
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
   };
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
     });
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/signup">← Go to Signup</Link>
-
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
+    <>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant="danger">
+          Something went wrong with your login credentials!
+        </Alert>
+        <Form.Group>
+          <Form.Label htmlFor="email">Email</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your email"
             name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
           />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  );
-}
+          <Form.Control.Feedback type="invalid">Email is required!</Form.Control.Feedback>
+        </Form.Group>
 
-export default Login;
+        <Form.Group>
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Your password"
+            name="password"
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type="invalid">Password is required!</Form.Control.Feedback>
+        </Form.Group>
+        <Button disabled={!(userFormData.email && userFormData.password)} type="submit" variant="success">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default LoginForm;
