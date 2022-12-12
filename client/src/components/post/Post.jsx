074678d73ffc64ea95b1100f79dac8acebbdab75
Valeria <alarconmvaleria@gmail.com>
2022-12-12 12:16:23 -0500

    import link from react router dom

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_POST, ADD_COMMENT } from '../../utils/mutations';
 import { QUERY_ME } from '../../utils/queries';
