52c2589a05155b0649d5fb44e28bbd1c60d95410
Valeria <alarconmvaleria@gmail.com>
2022-12-12 12:07:46 -0500

    QUERY_ME query imported from queries

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_POST, ADD_COMMENT } from '../../utils/mutations';
+import { QUERY_ME } from '../../utils/queries';
+
