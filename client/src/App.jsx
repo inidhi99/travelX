beda19c2724ddb94b9eb0f6fd3affc1728e5fceb
nidhi <inidhi99@gmail.com>
2022-12-12 11:46:09 -0500

    imported pages in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,16 @@ import Main from './components/main/Main';
 // import Explore from './components/explore/explore';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
+//importing pages 
+import Detail from './pages/Detail';
+import Explore from './pages/Explore'
+import Home from "./pages/Home";
+import Login from './pages/Login';
+import Post from './pages/Post';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -14,6 +24,44 @@ function App() {
       <Header />
       <Main className="mainSection" />
       <Footer />
+      <ApolloProvider client={client}>
+      <Router>
+        <div>
+       
+            <Nav />
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/explore" 
+                element={<Explore />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/post" 
+                element={<Post />} 
+              />
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
+              />
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
+              />
+            </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
     </div>
   );
 }
