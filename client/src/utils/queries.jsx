1c24b18b576ad0a8c4f517556cca048b8a1afc03
JAG <jagrasso42@gmail.com>
2022-12-13 18:41:16 -0500

    fixed the graphql syntax errors

--- client/src/utils/queries.jsx
+++ client/src/utils/queries.jsx
@@ -1,6 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const QUERY_USERS = gql`
+{  
   query allUsers {
     users {
       username
@@ -41,76 +42,25 @@ export const QUERY_USERS = gql`
       _id
     }
   }
+}
 `;
 
 export const QUERY_SINGLE_USER = gql`
-  query singleUSER($userId: ID!) {
-    user(username: $username) {
+{
+  query user(username: $username) {
+    _id
+    username
+    email
+    following {
       _id
       username
       email
-      following {
-        _id
-        username
-        email
-      }
-      followers {
-        _id
-        username
-        email
-      }
-      posts {
-        _id
-        userId
-        username
-        title
-        body
-        country
-        city
-        comments {
-          _id
-          userId
-          username
-          commentText
-          createdAt
-        }
-        reactions {
-          _id
-          userId
-          username
-          reactionType
-          createdAt
-        }
-      }
     }
-  }
-`;
-
-export const QUERY_POSTS = gql`
-<<<<<<< HEAD
-posts {
-  _id
-  body
-  city
-  country
-  title
-  userId
-  username
-  comments {
-    _id
-    userId
-    username
-    commentText
-  }
-  reactions {
-    _id
-    userId
-    username
-    reactionType
-  }
-}
-=======
-  query allPosts {
+    followers {
+      _id
+      username
+      email
+    }
     posts {
       _id
       userId
@@ -134,87 +84,116 @@ posts {
         createdAt
       }
     }
->>>>>>> 63473531e56166e4542a8ecf3b2081735a248c0f
   }
+}
+`;
+
+export const QUERY_POSTS = gql`
+{
+  posts {
+    _id
+    userId
+    username
+    title
+    body
+    country
+    city
+    comments {
+      _id
+      userId
+      username
+      commentText
+      createdAt
+    }
+    reactions {
+      _id
+      userId
+      username
+      reactionType
+      createdAt
+    }
+  }
+}
 `;
 
 export const QUERY_SINGLE_POST = gql`
-  query singlePost($postId: ID!) {
-    post(postId: $postId) {
+{  
+  query post(postId: $postId) {
+    _id
+    userId
+    username
+    title
+    body
+    country
+    city
+    comments {
       _id
       userId
       username
-      title
-      body
-      country
-      city
-      comments {
-        _id
-        userId
-        username
-        commentText
-        createdAt
-      }
-      reactions {
-        _id
-        userId
-        username
-        reactionType
-        createdAt
-      }
+      commentText
+      createdAt
+    }
+    reactions {
+      _id
+      userId
+      username
+      reactionType
+      createdAt
     }
   }
+}
 `;
 
 export const QUERY_COMMENTS = gql`
-  query allComments {
-    comments(postId: $postId) {
-      _id
-      userId
-      username
-      commentText
-      createdAt
-    }
+{
+  query comments(postId: $postId) {
+    _id
+    userId
+    username
+    commentText
+    createdAt
   }
+}
 `;
 
 export const QUERY_SINGLE_COMMENT = gql`
-  query singleComment($commentId: ID!) {
-    comment(commentId: $commentId) {
-      _id
-      userId
-      username
-      commentText
-      createdAt
-    }
+{
+  query comment(commentId: $commentId) {
+    _id
+    userId
+    username
+    commentText
+    createdAt
   }
+}
 `;
 
 export const QUERY_REACTIONS = gql`
-  query allReactions {
-    reactions(postId: $postId) {
-      _id
-      userId
-      username
-      reactionType
-      createdAt
-    }
+{
+  query reactions(postId: $postId) {
+    _id
+    userId
+    username
+    reactionType
+    createdAt
   }
+}
 `;
 
 export const QUERY_SINGLE_REACTION = gql`
-  query singleReaction($reactionId: ID!) {
-    reaction(reactionId: $reactionId) {
-      _id
-      userId
-      username
-      reactionType
-      createdAt
-    }
+{
+  query reaction(reactionId: $reactionId) {
+    _id
+    userId
+    username
+    reactionType
+    createdAt
   }
+}
 `;
 
 export const QUERY_ME = gql`
+{
   query me {
     me {
       _id
@@ -255,4 +234,5 @@ export const QUERY_ME = gql`
       }
     }
   }
+}
 `;
