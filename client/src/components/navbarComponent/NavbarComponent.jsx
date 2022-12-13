4e81eb372cb1bb6568a85bdb70613217b31e7f02
Valeria <alarconmvaleria@gmail.com>
2022-12-12 22:46:35 -0500

    navbar component styling

--- client/src/components/navbarComponent/NavbarComponent.jsx
+++ client/src/components/navbarComponent/NavbarComponent.jsx
@@ -2,19 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function NavbarComponent() {
   return (
     <>
       <Navbar bg="primary" variant="dark" fixed="bottom">
         <Container>
-          <Nav className="me-auto">
+          <Nav className="navbar">
             {/* Temporary */}
-            <Link className="linkStyles" to="./pages/explore">Explore</Link>
-            <Link className="linkStyles" to="./pages/create-post">Post</Link>
-            <Link className="linkStyles" to="./pages/profile">Profile</Link>
+            <Link className="link" to="./pages/explore">Explore</Link>
+            <Link className="link" to="./pages/create-post">Post</Link>
+            <Link className="link" to="./pages/profile">Profile</Link>
             {/* Login should only show if user isn't logged in */}
-            <Link className="linkStyles" to="./pages/login">Login</Link>
+            <Link className="link" to="./pages/login">Login</Link>
             
           </Nav>
         </Container>
