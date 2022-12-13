949f48a7cd7d6de66b4418d92028bd993522e61e
nidhi <inidhi99@gmail.com>
2022-12-13 10:51:43 -0500

    fixed the broken login page route

--- client/src/components/navbarComponent/NavbarComponent.jsx
+++ client/src/components/navbarComponent/NavbarComponent.jsx
@@ -1,21 +1,54 @@
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import { Link } from 'react-router-dom';
+// import './Navbar.css';
+
+// function NavbarComponent() {
+//   return (
+//     <>
+//       <Navbar bg="primary" variant="dark" fixed="bottom">
+//         <Container>
+//           <Nav className="navbar">
+//             {/* Temporary */}
+//             <Link className="link" to="./pages/explore">Explore</Link>
+//             <Link className="link" to="./pages/PostForm">Post</Link>
+//             <Link className="link" to="./pages/profile">Profile</Link>
+//             {/* Login should only show if user isn't logged in */}
+//             <Link className="link" to="/pages/LoginForm">Login</Link>
+            
+//           </Nav>
+//         </Container>
+//       </Navbar>
+//     </>
+//   );
+// }
+
+// export default NavbarComponent;
+
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
 
 function NavbarComponent() {
   return (
     <>
       <Navbar bg="primary" variant="dark" fixed="bottom">
         <Container>
-          <Nav className="navbar">
+          <Nav className="me-auto">
             {/* Temporary */}
-            <Link className="link" to="./pages/explore">Explore</Link>
-            <Link className="link" to="./pages/create-post">Post</Link>
-            <Link className="link" to="./pages/profile">Profile</Link>
-            {/* Login should only show if user isn't logged in */}
-            <Link className="link" to="./pages/login">Login</Link>
+            <Link className="linkStyles" to="./pages/postForm">Post</Link>
+            <Link className="linkStyles" to="./pages/Profile">Profile</Link>
+            <Link className="linkStyles" to="./pages/Explore">Explore</Link>
+            <Link className="linkStyles" to="./pages/PostForm">
+              Post
+            </Link>
+
+            <Link className="linkStyles" to="./pages/LoginForm">
+              login
+            </Link>
             
           </Nav>
         </Container>
