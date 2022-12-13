c1e82d9ded39fbc666a9330f6d9e0e800bc44249
JAG <jagrasso42@gmail.com>
2022-12-13 12:30:06 -0500

    added explore page content

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -1,12 +1,38 @@
-// import React from "react";
+import React, { useState, useEffect } from "react";
+import PostList from "../components/postList/PostList";
+import { useGlobalContext } from '../../context/globalContext';
+import { UPDATE_POSTS } from '../../context/actions';
+import { QUERY_POSTS } from '../../utils/queries';
+import SearchBox from "../components/SearchBox/search-box.component";
 
 
-// const Explore = () => {
-//   return (
-//     <div className="container">
-     
-//     </div>
-//   );
-// };
+const Explore = () => {
+  const [state, dispatch] = useGlobalContext();
+  const [searchField, setSearchField] = useState('');
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
-// export default Explore;
+  const { loading, data } = useQuery(QUERY_POSTS);
+    if (!state.posts.length) {
+        return <h3> No Posts Yet!</h3>
+    }
+
+    useEffect(() => {
+      const newFilteredPosts = data.filter(post => post.city.toLocaleLowerCase().includes(searchField));
+
+      setFilteredPosts(newFilteredPosts)
+    }, [data, searchField]);
+
+    const onSearchChange = e => {
+      const searchFieldString = e.target.value.toLocaleLowerCase();
+      setSearchField(searchFieldString);
+    };
+
+  return (
+    <div className="container">
+      <SearchBox className="posts-search-box" placeholder="search posts" onChangeHandler={onSearchChange} />
+      <PostList posts={filteredPosts} />
+    </div>
+  );
+};
+
+export default Explore;
