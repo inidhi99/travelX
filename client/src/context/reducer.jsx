ab5ede40e35147d778007f3a1aa9fae0ab37de25
JAG <jagrasso42@gmail.com>
2022-12-13 11:18:20 -0500

    add reducer and UPDATE_POSTS case

--- client/src/context/reducer.jsx
+++ client/src/context/reducer.jsx
@@ -1,6 +1,18 @@
-// UPDATE_POSTS
-export const UPDATE_POSTS = 'UPDATE_POSTS'
-// UPDATE_COMMENTS
-export const UPDATE_COMMENTS = 'UPDATE_COMMENTS'
-// UPDATE_REACTIONS
-export const UPDATE_REACTIONS = 'UPDATE_REACTIONS'
+import { useReducer } from 'react';
+import {
+  UPDATE_POSTS,
+  UPDATE_COMMENTS,
+  UPDATE_REACTIONS
+} from './actions'
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case UPDATE_POSTS:
+      return {
+        ...state,
+        posts: [...action.posts]
+      }
+    default:
+      return state;
+  }
+}
