c15011e38b7abce90b63b9f3fa70ee08f319d5eb
nidhi <inidhi99@gmail.com>
2022-12-13 10:57:21 -0500

    fixed the navbar css

--- client/src/components/navbarComponent/NavbarComponent.jsx
+++ client/src/components/navbarComponent/NavbarComponent.jsx
@@ -31,6 +31,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function NavbarComponent() {
   return (
