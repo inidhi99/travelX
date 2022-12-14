6779c378443ca425567a27cfab68eceee2c61497
nidhi <inidhi99@gmail.com>
2022-12-14 12:35:35 -0500

    login and signup form are working without any errors

--- client/src/components/navbarComponent/NavbarComponent.jsx
+++ client/src/components/navbarComponent/NavbarComponent.jsx
@@ -1,81 +1,62 @@
-import React, { useState } from 'react';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import { Link } from 'react-router-dom';
+// import './Navbar.css';
+
+// function NavbarComponent() {
+//   return (
+//     <>
+//       <Navbar bg="primary" variant="dark" fixed="bottom">
+//         <Container>
+//           <Nav className="navbar">
+//             {/* Temporary */}
+//             <Link className="link" to="./pages/explore">Explore</Link>
+//             <Link className="link" to="./pages/PostForm">Post</Link>
+//             <Link className="link" to="./pages/profile">Profile</Link>
+//             {/* Login should only show if user isn't logged in */}
+//             <Link className="link" to="/pages/LoginForm">Login</Link>
+
+//           </Nav>
+//         </Container>
+//       </Navbar>
+//     </>
+//   );
+// }
+
+// export default NavbarComponent;
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-import SignupForm from "../../pages/SignupForm";
-import LoginForm from "../../pages/LoginForm";
 import './Navbar.css';
-import Auth from '../../utils/auth';
 
-const NavbarComponent = () => {
-  // set modal display state
-  const [showModal, setShowModal] = useState(false);
-
+function NavbarComponent() {
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container fluid>
-          <Navbar.Brand className="linkStyles" as={Link} to="./pages/LoginForm">
-           Explore
-          </Navbar.Brand>
-          <Navbar.Brand className="linkStyles" as={Link} to="./pages/LoginForm">
-          Post
-          </Navbar.Brand>
-          <Navbar.Brand className="linkStyles" as={Link} to="./pages/LoginForm">
-          Profile
-          </Navbar.Brand> 
-         
-         {Auth.loggedIn() ? (
-                <>
-                  <Navbar.Brand className="linkStyles" as={Link} to="./pages/Explore">
-           Explore
-          </Navbar.Brand>
-          <Navbar.Brand className="linkStyles" as={Link} to="./pages/postForm">
-          Post
-          </Navbar.Brand>
-          <Navbar.Brand className="linkStyles" as={Link} to="./pages/Profile">
-          Profile
-          </Navbar.Brand>
-          <Nav.Link className="linkStyles" onClick={Auth.logout}>Logout</Nav.Link>
-                </>
+      <Navbar bg="primary" variant="dark" fixed="bottom">
+        <Container>
+          <Nav className="me-auto">
+            {/* <Link className="linkStyles" to="./pages/postForm">Post</Link> */}
+            <Link className="linkStyles" to="./pages/Profile">
+              Profile
+            </Link>
+            <Link className="linkStyles" to="./pages/Explore">
+              Explore
+            </Link>
+            <Link className="linkStyles" to="./pages/PostForm">
+              Post
+            </Link>
 
-              ) : (
-                <Nav.Link className="linkStyles" onClick={() => setShowModal(true)} to="./pages/LoginForm" >Login/Sign Up</Nav.Link>
-              )}
-           
+            <Link className="linkStyles" to="./pages/LoginForm">
+              login
+            </Link>
+          </Nav>
         </Container>
       </Navbar>
-      {/* set modal data up */}
-      <Modal  className="linkStyles" size="lg" show={showModal} onHide={() => setShowModal(false)} aria-labelledby="signup-modal">
-        {/* tab container to do either signup or login component */}
-        <Tab.Container  defaultActiveKey="login">
-          <Modal.Header className="linkStyles" closeButton>
-            <Modal.Title id="signup-modal">
-              <Nav variant="pills">
-                <Nav.Item>
-                  <Nav.Link className="linkStyles" eventKey="login">Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link className="linkStyles" eventKey="signup">Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-
-
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey="login">
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              {/* <Tab.Pane eventKey="signup">
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane> */}
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
     </>
   );
-};
+}
 
 export default NavbarComponent;
--- client/src/pages/SignupForm.jsx
+++ client/src/pages/SignupForm.jsx
@@ -1,166 +1,87 @@
-import React from 'react';
-
-import { Container, Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
 
-const SignupForm = () => {
+function Signup(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email,
+        password: formState.password,
+        firstName: formState.firstName,
+        lastName: formState.lastName,
+      },
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
   return (
-    <>
-       
-       <p>This is the Sign up page</p>
+    <div className="container my-1">
+      <Link to="/pages/LoginForm">← Go to Login</Link>
 
-       <Container id="main-container" className="d-grid h-100">
-      <Form id="sign-in-form" className="text-center p-3 w-100">
-    
-        <h1 className="mb-3 fs-3 fw-normal">Please Sign Up</h1>
-
-        <Form.Group controlId="sign-in-first-name"> First Name
-          <Form.Control type="name"  placeholder=" First Name " name="username" />
-        </Form.Group>
-
-        <Form.Group controlId="sign-in-last-name"> Last Name
-          <Form.Control type="text"  placeholder=" Last Name " name="username" />
-        </Form.Group>
-
-        <Form.Group controlId="sign-in-username">@ 
-        <Form.Control
-          placeholder="Username"
-          aria-label="Username"
-          aria-describedby="basic-addon1"
-        />
-        </Form.Group>
-
-
-        <Form.Group controlId="sign-in-email-address"> Email Address
-          <Form.Control type="email"  placeholder="Email" name="username" />
-        </Form.Group>
-
-
-        <Form.Group className="mb-3" controlId="sign-in-password"> Password
-          <Form.Control type="password" size="lg" placeholder="Password" autoComplete="current-password" className="position-relative" />
-        </Form.Group>
-
-        <Link className="linkStyles" to="/explore">
-        <Button variant="primary" size="lg"> Sign UP </Button>
-        </Link>
-
-
-      </Form>
-    </Container>
-
-    </>
-
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            placeholder="First"
+            name="firstName"
+            type="firstName"
+            id="firstName"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            placeholder="Last"
+            name="lastName"
+            type="lastName"
+            id="lastName"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit" to="/explore">Submit</button>
+        </div>
+      </form>
+    </div>
   );
-};
+}
 
-export default SignupForm;
-
-// import React, { useState } from 'react';
-// import { Form, Button, Alert } from 'react-bootstrap';
-
-
-// import { ADD_USER } from '../utils/mutations';
-// import Auth from '../utils/auth';
-// import { useMutation } from '@apollo/client';
-
-// const SignupForm = () => {
-//   // set initial form state
-//   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-//   // set state for form validation
-//   const [signup, { error }] = useMutation(ADD_USER);
-//   const [validated] = useState(false);
-//   // set state for alert
-//   const [showAlert, setShowAlert] = useState(false);
-
-//   const handleInputChange = event => {
-//     const { name, value } = event.target;
-//     setUserFormData({ ...userFormData, [name]: value });
-//   };
-
-//   const handleFormSubmit = async event => {
-//     event.preventDefault();
-
-//     // check if form has everything (as per react-bootstrap docs)
-//     const form = event.currentTarget;
-//     if (form.checkValidity() === false) {
-//       event.preventDefault();
-//       event.stopPropagation();
-//     }
-
-//     try {
-//       const { data } = await signup({
-//         variables: { ...userFormData },
-//       });
-//       Auth.login(data.signup.token);
-//     } catch (err) {
-//       console.error(err);
-//       setShowAlert(true);
-//     }
-
-//     setUserFormData({
-//       username: '',
-//       email: '',
-//       password: '',
-//     });
-//   };
-
-//   return (
-//     <>
-//       {/* This is needed for the validation functionality above */}
-//       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-//         {/* show alert if server response is bad */}
-//         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant="danger">
-//           Something went wrong with your signup!
-//         </Alert>
-
-//         <Form.Group>
-//           <Form.Label htmlFor="username">Username</Form.Label>
-//           <Form.Control
-//             type="text"
-//             placeholder="Your username"
-//             name="username"
-//             onChange={handleInputChange}
-//             value={userFormData.username}
-//             required
-//           />
-//           <Form.Control.Feedback type="invalid">Username is required!</Form.Control.Feedback>
-//         </Form.Group>
-
-//         <Form.Group>
-//           <Form.Label htmlFor="email">Email</Form.Label>
-//           <Form.Control
-//             type="email"
-//             placeholder="Your email address"
-//             name="email"
-//             onChange={handleInputChange}
-//             value={userFormData.email}
-//             required
-//           />
-//           <Form.Control.Feedback type="invalid">Email is required!</Form.Control.Feedback>
-//         </Form.Group>
-
-//         <Form.Group>
-//           <Form.Label htmlFor="password">Password</Form.Label>
-//           <Form.Control
-//             type="password"
-//             placeholder="Your password"
-//             name="password"
-//             onChange={handleInputChange}
-//             value={userFormData.password}
-//             required
-//           />
-//           <Form.Control.Feedback type="invalid">Password is required!</Form.Control.Feedback>
-//         </Form.Group>
-//         <Button
-//           disabled={!(userFormData.username && userFormData.email && userFormData.password)}
-//           type="submit"
-//           variant="success"
-//         >
-//           Submit
-//         </Button>
-//       </Form>
-//     </>
-//   );
-// };
-
-// export default SignupForm;
+export default Signup;
