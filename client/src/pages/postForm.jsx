97be1399b2783c1b3072220e28924b09b8f20fae
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-13 21:26:49 -0500

    submit btn styling

--- client/src/pages/postForm.jsx
+++ client/src/pages/postForm.jsx
@@ -3,21 +3,15 @@ import { useMutation } from '@apollo/client';
 import { ADD_POST } from '../utils/mutations';
 import axios from 'axios';
 import Images from '../components/images/Images';
-import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
+
+import Container from 'react-bootstrap/Container';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
-
-// Upload photo
-// add description
-// add location - country city
-// upload image
-// date
-// comments
-// reactions
 
 const PostForm = () => {
   // Unsplash image API
@@ -42,7 +36,8 @@ const PostForm = () => {
   const [city, setCity] = useState('');
   const [body, setBody] = useState('');
 
-  const [addPost, { error }] = useMutation(ADD_POST);
+  // this is breaking
+  // const [addPost, { error }] = useMutation(ADD_POST);
 
   // form submit function
   const handleFormSubmit = async (e) => {
@@ -62,98 +57,98 @@ const PostForm = () => {
     setCity('');
     setBody('');
   };
-  // const [data, setData] = useState();
-  // const [print, setPrint] = useState(false);
-  // const getData = (e) => {
-  //   setData(e.target.value);
-  //   console.log(e.target.value);
-  // };
+
   return (
     <>
-      {/* <CssBaseline /> */}
-      <Grid container>
-        {/* Image list  */}
-        <ImageList
-          sx={{
-            width: 500,
-            height: 500,
-            overflow: 'hidden',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            zIndex: 2,
-          }}
-        >
-          <ImageListItem>
-            <button className="btn btn-primary btn-sm" onClick={fetchAPI}>
-              Click
-            </button>
-            <div className="photos">
-              {images.length > 0 && <Images images={images} />}
-            </div>
-          </ImageListItem>
-        </ImageList>
-      </Grid>
+      <Container id="main-container" className="d-grid h-100">
+        <Grid container>
+          <ImageList
+            sx={{
+              width: 500,
+              height: 500,
+              overflow: 'hidden',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              zIndex: 2,
+            }}
+          >
+            <ImageListItem>
+              <button className="btn btn-primary btn-sm" onClick={fetchAPI}>
+                Click
+              </button>
+              <div className="photos">
+                {images.length > 0 && <Images images={images} />}
+              </div>
+            </ImageListItem>
+          </ImageList>
+        </Grid>
 
-      {/* Input Form */}
-      <FormControl sx={{ width: '25ch' }}>
-        <TextField
-          input={title}
-          name="title"
-          onChange={(e) => setTitle(e.target.value)}
-          type="text"
-          placeholder="Enter A Title..."
-          id="standard-basic"
-          variant="standard"
-        />
-        <TextField
-          input={country}
-          name="country"
-          onChange={(e) => setCountry(e.target.value)}
-          type="text"
-          placeholder="Provide A Country"
-          id="standard-basic"
-          variant="standard"
-        />
-        <TextField
-          input={city}
-          name="city"
-          onChange={(e) => setCity(e.target.value)}
-          type="text"
-          placeholder="Provide A City"
-          id="standard-basic"
-          variant="standard"
-        />
-        <Box
-          component="form"
-          sx={{
-            '& .MuiTextField-root': {
-              m: 1,
-              width: '30ch',
-            },
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          noValidate
-          autoComplete="off"
-        >
+        {/* Input Form */}
+        <FormControl>
           <TextField
-            input={body}
-            name="body"
+            input={title}
+            name="title"
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            placeholder="Enter A Title..."
+            id="standard-basic"
+            variant="standard"
+          />
+          <TextField
+            input={country}
+            name="country"
+            onChange={(e) => setCountry(e.target.value)}
+            type="text"
+            placeholder="Provide A Country"
+            id="standard-basic"
+            variant="standard"
+          />
+          <TextField
+            input={city}
+            name="city"
             onChange={(e) => setCity(e.target.value)}
             type="text"
-            placeholder="Enter a post"
-            sx={{ p: 0.5 }}
-            id="outlined-multiline-static"
-            multiline
-            rows={4}
+            placeholder="Provide A City"
+            id="standard-basic"
+            variant="standard"
           />
-        </Box>
-        <button className="btn btn-primary btn-sm" onClick={handleFormSubmit}>
-          Submit
-        </button>
-      </FormControl>
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': {
+                m: 1,
+                width: '30ch',
+              },
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              input={body}
+              name="body"
+              onChange={(e) => setCity(e.target.value)}
+              type="text"
+              placeholder="Enter a post"
+              sx={{ p: 0.5 }}
+              id="outlined-multiline-static"
+              multiline
+              rows={4}
+            />
+          </Box>
+          <Button
+            className="submitBtn"
+            variant="contained"
+            size="small"
+            onClick={handleFormSubmit}
+          >
+            Submit
+          </Button>
+        </FormControl>
+      </Container>
     </>
   );
 };
