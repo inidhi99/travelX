12a93fcfae3dd6fe7a0b9592cf7cef09d8adbb64
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-14 12:22:06 -0500

    post form mutations

--- client/src/utils/mutations.jsx
+++ client/src/utils/mutations.jsx
@@ -1,93 +1,126 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-{
-  mutation login(password: $password, username: $username) {
-    token
-    user {
-      _id
-      username
+  {
+    mutation
+    login(password: $password, username: $username) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
-}
 `;
 
 export const ADD_USER = gql`
-{
-  mutation addUser(username: $username, password: $password, email: $email) {
-    token
-    user {
-      _id
-      username
+  {
+    mutation
+    addUser(username: $username, password: $password, email: $email) {
+      token
+      user {
+        _id
+        username
+      }
     }
   }
-}
 `;
 
+// export const ADD_POST = gql`
+// {
+//   mutation addPost(title: $title, body: $body, city: $city, country: $country) {
+//     _id
+//     title
+//     body
+//     country
+//     city
+//   }
+// }
+// `;
+
+// export const ADD_POST = gql`
+//   {
+//     mutation
+//     addPost(title: $title, body: $body, city: $city, country: $country) {
+//       _id
+//       title
+//       body
+//       city
+//       country
+//     }
+//   }
+// `;
 export const ADD_POST = gql`
-{
-  mutation addPost(title: $title, body: $body, city: $city, country: $country) {
-    _id
-    title
-    body
-    country
-    city
+  mutation addPost(
+    $title: String
+    $body: String
+    $city: String
+    $country: String
+  ) {
+    addPost(title: $title, body: $body, city: $city, country: $country) {
+      _id
+      title
+      body
+      city
+      country
+    }
   }
-}
 `;
 
 export const ADD_COMMENT = gql`
-{
-  mutation addComment(postId: $postId, commentText: $commentText) {
-    _id
-    userId
-    username
-    title
-    body
-    country
-    city
-    comments {
+  {
+    mutation
+    addComment(postId: $postId, commentText: $commentText) {
       _id
-      username
       userId
-      createdAt
-      commentText
-    }
-    reactions {
-      _id
       username
-      userId
-      reactionType
-      createdAt
+      title
+      body
+      country
+      city
+      comments {
+        _id
+        username
+        userId
+        createdAt
+        commentText
+      }
+      reactions {
+        _id
+        username
+        userId
+        reactionType
+        createdAt
+      }
     }
   }
-}
 `;
 
 export const ADD_REACTION = gql`
-{
-  mutation addReaction(postId: $postId, reactionType: $reactionType) {
-    _id
-    userId
-    username
-    title
-    body
-    country
-    city
-    comments {
+  {
+    mutation
+    addReaction(postId: $postId, reactionType: $reactionType) {
       _id
       userId
       username
-      commentText
-      createdAt
-    }
-    reactions {
-      _id
-      userId
-      username
-      reactionType
-      createdAt
+      title
+      body
+      country
+      city
+      comments {
+        _id
+        userId
+        username
+        commentText
+        createdAt
+      }
+      reactions {
+        _id
+        userId
+        username
+        reactionType
+        createdAt
+      }
     }
   }
-}
 `;
