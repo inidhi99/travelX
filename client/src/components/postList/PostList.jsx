8385f37498ecc00d9f6bd0ff943757af72f6b192
JAG <jagrasso42@gmail.com>
2022-12-14 20:49:09 -0500

    add turnary statement to check if there are posts

--- client/src/components/postList/PostList.jsx
+++ client/src/components/postList/PostList.jsx
@@ -6,7 +6,7 @@ import Post from '../post/Post';
 const PostList = ({ posts }) => {
     return (
       <div id="post-container" className='posts-list'>
-        {posts.map(post => <Post key={post._id} post={post} />)}
+        {posts ? posts.map(post => <Post key={post._id} post={post} />) : <p>No Posts Yet</p>}
       </div>
     )
 };
