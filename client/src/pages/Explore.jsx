7413869c0d1da2cac20ccfbf3f5216bf91c55bfb
Igor Korman <ikorman12@gmail.com>
2022-12-14 21:36:20 -0500

    merge

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -1,37 +1,41 @@
 import React, { useState, useEffect } from "react";
 import PostList from "../components/postList/PostList";
-import { useQuery } from '@apollo/client'
-import { useGlobalContext } from '../context/globalContext';
-import { UPDATE_POSTS } from '../context/actions';
-import { QUERY_POSTS } from '../utils/queries';
+import { useQuery } from "@apollo/client";
+import { useGlobalContext } from "../context/globalContext";
+import { UPDATE_POSTS } from "../context/actions";
+import { QUERY_POSTS } from "../utils/queries";
 import SearchBox from "../components/SearchBox/search-box.component";
-
 
 const Explore = () => {
   const [state, dispatch] = useGlobalContext();
-  const [searchField, setSearchField] = useState('');
+  const [searchField, setSearchField] = useState("");
   const [filteredPosts, setFilteredPosts] = useState([]);
 
   const { loading, data } = useQuery(QUERY_POSTS);
 
-    useEffect(() => {
-      if (data) {
-        const newFilteredPosts = data.posts.filter(post => post.city.toLocaleLowerCase().includes(searchField));
+  useEffect(() => {
+    if (data) {
+      const newFilteredPosts = data.posts.filter((post) =>
+        post.city.toLocaleLowerCase().includes(searchField)
+      );
 
-        setFilteredPosts(newFilteredPosts)
-      }
+      setFilteredPosts(newFilteredPosts);
+    }
+  }, [data, searchField]);
 
-    }, [data, searchField]);
-
-    const onSearchChange = e => {
-      const searchFieldString = e.target.value.toLocaleLowerCase();
-      setSearchField(searchFieldString);
-    };
+  const onSearchChange = (e) => {
+    const searchFieldString = e.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
 
   return (
     <div className="container">
-      Explore the World!!
-      <SearchBox className="posts-search-box" placeholder="search posts" onChangeHandler={onSearchChange} />
+      <h2 className="expTitle">Explore the World!!</h2>
+      <SearchBox
+        className="posts-search-box"
+        placeholder="search posts"
+        onChangeHandler={onSearchChange}
+      />
       <PostList posts={filteredPosts} />
     </div>
   );
