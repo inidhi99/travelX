6f5555ae13f5cbd1a2d134c4a48793c28f6d4557
Jules Joseph <julesj93@gmail.com>
2022-12-14 19:53:16 -0500

    imported bootsrap

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -26,7 +26,7 @@ const Post = ({ post }) => {
       <div className="post-image"><Card.Img variant="top" src={image} /></div>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Text className="text-light">
+        <Card.Text className="text-area">
 		<span>{city}, {country}</span>
 		<div>{body}</div>
 		<div>{createdAt}</div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-import './components/post/Post.css'
-
->>>>>>> c7f2f60b4b833a399c981f14d52f96806db25dfc
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
