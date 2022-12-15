be488fa9689ae944909127106ddf58becbd12f82
briabr <filalibria@yahoo.fr>
2022-12-14 22:22:50 -0500

    added more user seeds

--- server/seeders/commentSeeds.js
+++ server/seeders/commentSeeds.js
@@ -6,7 +6,79 @@ module.exports = [
         commentText:"Awesome!!",
     },
     {
-        commentText:"I drow up near here!",
+        commentText:"I grew up near here!",
+    },
+    {
+        commentText:"I'm gnna put this on bucket list",
+    },
+    {
+        commentText:"Love it!",
+    },
+    {
+        commentText:"Splendid!",
+    },
+    {
+        commentText:"I only visit American cities",
+    },
+    {
+        commentText:"Love your photo!!",
+    },
+    {
+        commentText:"Cool!",
+    },
+    {
+        commentText:"Awesome!!",
+    },
+    {
+        commentText:"I grew up near here!",
+    },
+    {
+        commentText:"I'm gnna put this on bucket list",
+    },
+    {
+        commentText:"Love it!",
+    },
+    {
+        commentText:"Splendid!",
+    },
+    {
+        commentText:"I only visit American cities",
+    },
+    {
+        commentText:"Love your photo!!",
+    },
+    {
+        commentText:"Cool!",
+    },
+    {
+        commentText:"Awesome!!",
+    },
+    {
+        commentText:"I grew up near here!",
+    },
+    {
+        commentText:"I'm gnna put this on bucket list",
+    },
+    {
+        commentText:"Love it!",
+    },
+    {
+        commentText:"Splendid!",
+    },
+    {
+        commentText:"I only visit American cities",
+    },
+    {
+        commentText:"Love your photo!!",
+    },
+    {
+        commentText:"Cool!",
+    },
+    {
+        commentText:"Awesome!!",
+    },
+    {
+        commentText:"I grew up near here!",
     },
     {
         commentText:"I 'm gnna put this on bucket list",
@@ -30,79 +102,7 @@ module.exports = [
         commentText:"Awesome!!",
     },
     {
-        commentText:"I drow up near here!",
-    },
-    {
-        commentText:"I 'm gnna put this on bucket list",
-    },
-    {
-        commentText:"Love it!",
-    },
-    {
-        commentText:"Splendid!",
-    },
-    {
-        commentText:"I only visit American cities",
-    },
-    {
-        commentText:"Love your photo!!",
-    },
-    {
-        commentText:"Cool!",
-    },
-    {
-        commentText:"Awesome!!",
-    },
-    {
-        commentText:"I drow up near here!",
-    },
-    {
-        commentText:"I 'm gnna put this on bucket list",
-    },
-    {
-        commentText:"Love it!",
-    },
-    {
-        commentText:"Splendid!",
-    },
-    {
-        commentText:"I only visit American cities",
-    },
-    {
-        commentText:"Love your photo!!",
-    },
-    {
-        commentText:"Cool!",
-    },
-    {
-        commentText:"Awesome!!",
-    },
-    {
-        commentText:"I drow up near here!",
-    },
-    {
-        commentText:"I 'm gnna put this on bucket list",
-    },
-    {
-        commentText:"Love it!",
-    },
-    {
-        commentText:"Splendid!",
-    },
-    {
-        commentText:"I only visit American cities",
-    },
-    {
-        commentText:"Love your photo!!",
-    },
-    {
-        commentText:"Cool!",
-    },
-    {
-        commentText:"Awesome!!",
-    },
-    {
-        commentText:"I drow up near here!",
+        commentText:"I grew up near here!",
     },
     {
         commentText:"I 'm gnna put this on bucket list",
