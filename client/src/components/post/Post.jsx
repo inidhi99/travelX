9444ca9028829416041e65e024b9ef1cd82d9fa2
Jules Joseph <julesj93@gmail.com>
2022-12-14 22:41:22 -0500

    added colored text

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -52,26 +52,6 @@ const Post = ({ post }) => {
 	}
 
 	return (
-<<<<<<< HEAD
-  <Card >
-		<Link to={`/profiles/${username}`}>{username}</Link>
-      <div className="post-image"><Card.Img variant="top" src={image} /></div>
-      <Card.Body >
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>
-		<span>{city}, {country}</span>
-		<div>{body}</div>
-		<div>{createdAt}</div>
-        </Card.Text>
-        <Card.Text>
-					<span>{body}</span>
-				</Card.Text>
-        <Card.Text>
-					<span>{createdAt}</span>
-				</Card.Text>
-      </Card.Body>
-    </Card>
-=======
 		<>
 			<Card >
 				<Link to={`/profiles/${username}`}>{username}</Link>
@@ -119,7 +99,6 @@ const Post = ({ post }) => {
         </Modal.Footer>
       </Modal>
 		</>
->>>>>>> 76742ea42b8cf5093e6e087f29f4793d645e4806
 	)
 };
 
